a72c084aee9cbcc77f9290e50faf590b435c8e1a
Aqsa Umair <hafizaaqsa42@gmail.com>
2024-08-08 14:00:32 +0500

    remove commented code

--- index.js
+++ index.js
@@ -170,14 +170,6 @@ app.delete('/api/persons/:id', (request,response,next) => {
 //add a person
 
 const errorHandler = (error, request, response, next) => {
-  debugger
-  // if (error.name === 'CastError') {
-  //   return response.status(400).send({ error: 'malformatted id' })
-  // } else if (error.name === 'ValidationError') {
-  //   return response.status(400).json({ error: error.message })
-  // }
-
-  // next(error)
   console.error(error.message);
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
