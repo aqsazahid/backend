0f9eb6de582fcbc17df8df83623e728fd7311ca0
Aqsa Umair <hafizaaqsa42@gmail.com>
2024-08-09 12:51:46 +0500

    oart 4.4 completed

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -7,3 +7,55 @@ describe('average', () => {
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)})
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17fa',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17fb',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+      likes: 10,
+      __v: 0
+    }
+  ]
+  const listWithNoBlogs = []
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+  test('when list has multiple blogs, equals the sum of likes', () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs)
+    assert.strictEqual(result, 29) // 7 + 12 + 10 = 29
+  })
+  test('when list is empty, equals zero', () => {
+    const result = listHelper.totalLikes(listWithNoBlogs)
+    assert.strictEqual(result, 0)
+  })
+})
