f33939a427b761fed170514602ad931ee933c3f0
Aqsa Umair <hafizaaqsa42@gmail.com>
2024-08-13 12:36:19 +0500

    part 4.20 completed

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/user')
 const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
+const tokenExtractor = require('./middleware/tokenExtractor')
+
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -26,8 +28,8 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(tokenExtractor)
 app.use(middleware.requestLogger)
-
 app.use('/api/notes', notesRouter)
 app.use('/api/persons', personRouter)
 app.use('/api/blogs', blogsRouter)
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,20 +2,13 @@ const blogsRouter = require('express').Router()
 const jwt = require('jsonwebtoken')
 const Blog = require('../models/blogs')
 const  User = require('../models/user')
-const getTokenFrom = request => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
 
 blogsRouter.post('/', async (req, res) => {
   const { title, author, url, likes = 0 } = req.body
-  const token = getTokenFrom(req)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  const decodedToken = jwt.verify(req.token, process.env.SECRET)
   if (!decodedToken.id) {
-    return res.status(401).json({ error: 'token missing or invalid' });
+    return res.status(401).json({ error: 'token invalid' })
   }
   const user = await User.findOne()
   if (!title || !url) {
