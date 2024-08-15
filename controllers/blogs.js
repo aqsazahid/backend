b2f069e2f588018debea1ea9bada9d5fcc71e782
Aqsa Umair <hafizaaqsa42@gmail.com>
2024-08-15 16:51:08 +0500

    part 5.11 completed

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,36 +3,10 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const Blog = require('../models/blogs')
 const  User = require('../models/user')
+const middleware = require('../utils/middleware')
 
-// blogsRouter.post('/', async (req, res) => {
-//   const { title, author, url, likes = 0 } = req.body
-
-//   const decodedToken = jwt.verify(req.token, process.env.SECRET)
-
-//   if (!decodedToken.id) {
-//     return res.status(401).json({ error: 'token invalid' })
-//   }
-//   const user = await User.findById(decodedToken.id)
-//   if (!title || !url) {
-//     return res.status(400).json({ error: 'title and url are required' })
-//   }
-//   try {decodedToken
-//     const blog = new Blog({
-//       title,
-//       author,
-//       url,
-//       likes: likes || 0,
-//       user: user._id
-//     })
-//     const savedBlog = await blog.findById(savedBlog._id).populate('user', { username: 1, name: 1 });
-//     user.blogs = user.blogs.concat(savedBlog._id)
-//     await user.save()
-//     res.status(201).json(savedBlog)
-//   } catch (error) {
-//     res.status(400).json({ error: 'unable to save the blog post' })
-//   }
-// })
-
+blogsRouter.use(middleware.tokenExtractor)
+blogsRouter.use(middleware.userExtractor)
 blogsRouter.post('/', async (req, res) => {
   const { title, author, url, likes = 0 } = req.body
 
@@ -67,8 +41,6 @@ blogsRouter.post('/', async (req, res) => {
   }
 })
 
-
-
 blogsRouter.put('/:id', async (req, res) => {
   const { id } = req.params
   const { title, author, url, likes } = req.body
@@ -97,16 +69,19 @@ blogsRouter.get('/', async (req, res) => {
   }
 })
 
+
 blogsRouter.delete('/:id', async (req, res) => {
   const user = req.user
   const blog = await Blog.findById(req.params.id)
   if (!blog) {
     return res.status(404).json({ error: 'blog not found' })
   }
+
   if (blog.user.toString() !== user._id.toString()) {
-    return res.status(403).json({ error: 'only the creator can delete this blog' });
+    return res.status(403).json({ error: 'only the creator can delete this blog' })
   }
-  await blog.remove()
+
+  await Blog.deleteOne({ _id: req.params.id })
   res.status(204).end()
 })
 
